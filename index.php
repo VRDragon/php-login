aedb7cf0278f8700bff25736dd0a7d50b4136361
VRDragon <30727840+VRDragon@users.noreply.github.com>
2017-08-20 15:55:28 +0200

    test
    test

--- index.php
+++ index.php
@@ -32,21 +32,8 @@
 <meta charset="UTF-8">
 <title>Ethernet 16 Relay Control</title>
 <style>
-.boxon{
-background-color:#0f0;
-margin:1px auto;
-color:#fff;
-font-size:30px;
-font:Cambria, "Hoefler Text", "Liberation Serif", Times, "Times New Roman", serif;
-border-radius:10px;
-border:2px solid #ccc;
-border-color:#0f0;
-padding:1px 100px 1px;
-display:inline-block;
-position: relative;
-}
-.boxoff{
-background-color:#f00;
+.box{
+background-color:#000;
 margin:1px auto;
 color:#fff;
 font-size:30px;
@@ -155,7 +142,7 @@ color:#0f0;
 	echo("<div class='login'>");
 	}
 		if ($val_array[$i][0] == 0 ) {
-			echo("<div class='boxoff'>");
+			echo("<div class='box'>");
 			echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".jpg' align='left' height='50' width='50' onclick='change_pin (".$i.");'/>");
 			echo("svietidlo .$i ");
 			echo("<br />");
@@ -163,7 +150,7 @@ color:#0f0;
 		}
 		//if on
 		if ($val_array[$i][0] == 1 ) {
-			echo("<div class='boxon' >");
+			echo("<div class='box'>");
 			echo ("<img id='button_".$i."' src='data/img/green/green_".$i.".jpg' align='left' height='50' width='50' onclick='change_pin (".$i.");'/>");
 			echo("svietidlo .$i ");
 			echo("<br />");
