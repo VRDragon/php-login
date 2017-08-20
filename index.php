0d37d1adf336074d704c024991cfe039b2f4f8c5
VRDragon <30727840+VRDragon@users.noreply.github.com>
2017-08-20 14:30:17 +0200

    g
    g

--- index.php
+++ index.php
@@ -44,13 +44,12 @@ display:inline-block;
 position: relative;
 }
 .login{
-background-color:#000;
 margin: calc(5%);
 margin-right: calc(0.5%);
 margin-left: calc(0.5%);
 font:Cambria, "Hoefler Text", "Liberation Serif", Times, "Times New Roman", serif;
 border-radius:10px;
-border:2px solid #ccc;
+border:5px solid #ccc;
 border-color:#0f0;
 padding:10px 40px 25px;
 margin-right: calc(0.5%);
